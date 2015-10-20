f713edf0a57241daafef20959968bbc714467420
Daan Flore <daan.flore.u8852@student.hogent.be>
2015-10-20 18:23:06 +0200

    push express

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,89 @@
+var app = angular.module('flapperNews', ['ui.router']);
+
+app.controller('MainCtrl', [
+    '$scope',
+    'posts',
+    function($scope, posts){
+        $scope.posts = posts.posts;
+
+        $scope.addPost = function(){
+            if(!$scope.title || $scope.title === '') { return; }
+            posts.create({
+                title: $scope.title,
+                link: $scope.link
+            });
+            $scope.title="";
+            $scope.link="";
+        };
+
+       $scope.incrementUpvotes = function(post) {
+           post.upvotes += 1;
+       };
+    }]);
+
+app.factory('posts', ['$http', function($http) {
+    var postFactory = {
+        posts: [
+            //{title: 'post 1', upvotes: 5},
+            //{title: 'post 2', upvotes: 2},
+            //{title: 'post 3', upvotes: 8},
+            //{title: 'post 4', upvotes: 9},
+            //{title: 'post 5', upvotes: 10}
+        ]
+    };
+
+    postFactory.getAll = function() {
+        return $http.get('/posts').success(function(data){
+            angular.copy(data, postFactory.posts);
+        });
+    };
+
+    postFactory.create = function(post){
+        return $http.post('/posts', post).success(function(data){
+            postFactory.posts.push(data);
+        });
+    };
+
+    return postFactory;
+}]);
+
+app.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+           url: '/home',
+           templateUrl: '/home.html',
+           controller: 'MainCtrl',
+           resolve: {
+            postPromise: ['posts', function(posts) {
+                return posts.getAll();
+            }]
+           }
+        })
+        .state('posts', {
+            url: '/posts/{id}',
+            templateUrl: '/posts.html',
+            controller: 'PostCtrl'
+        });
+
+
+        $urlRouterProvider.otherwise('home');
+    }]);
+
+app.controller('PostCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts) {
+        $scope.post = posts.posts[$stateParams.id];
+        $scope.addComment = function() {
+            if($scope.body === '') {return;}
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes: 0
+            });
+            $scope.body = '';
+        };
+    }]);
